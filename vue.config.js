3124f551006ed10be3f616b53bfedcb9a4bdbd59
Ofilio Herdocia Aráuz <herdocia20@gmail.com>
2022-05-05 21:56:09 -0600

    Revert "Configurado Vuetify."
    This reverts commit c60b53b014cf831460a8f400b3b57eafaf9da4c8.

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-
   pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+    },
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+});
